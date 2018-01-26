9315a016be7d87512cf850143535c8699775412e
songxiaoxu <913581148@qq.com>
2018-01-26 09:25:24 +0800

    微信浏览器打开顶部提示：到外部浏览器打开

--- phiHealth-pages/phicomm/js/common/footerDown.js
+++ phiHealth-pages/phicomm/js/common/footerDown.js
@@ -1,4 +1,25 @@
 (function () {
+    var search = window.location.search;
+    var arrsign = search.substr(1).split("&");
+    var searchobj=new Object({});
+    for(var i=0;i<arrsign.length;i++){
+        searchobj[arrsign[i].split("=")[0]]=encodeURI(arrsign[i].split("=")[1]);
+    }
+    function isWeiXin() {
+        var ua = window.navigator.userAgent.toLowerCase();
+        if (ua.match(/MicroMessenger/i) == 'micromessenger') {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    if(isWeiXin()){
+        document.getElementsByClassName("headertop")[0].style.display='block';
+    }else{
+        if(searchobj.Shareflag && searchobj.Shareflag.toString() === "1"){
+            document.getElementsByClassName("footersticky")[0].style.display='flex';
+        }
+    }
     document.getElementsByClassName("callbackthis")[0].addEventListener("click", function() {
         window.location.href = "scheme://phicomm.phicare/awaken/html/scheme?name=google&page=2";
         window.setTimeout(function() {
